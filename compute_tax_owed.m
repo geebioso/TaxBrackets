c69a2655349bf1db6d6be2458d2d9fc6b9c3c5d2
Garren Gaut <gbius06@gmail.com>
2017-11-06 14:42:53 -0600

    commented code

--- compute_tax_owed.m
+++ compute_tax_owed.m
@@ -6,17 +6,22 @@ if salary < pep
     salary = salary - deduct; 
 end
 
+% take personal examption 
 salary = salary - pexemp; 
 
+% if the deduction plus exemption are greater than or equal taxes owed, set salary
+% to 0 and set bracket to lowest 
 if salary <= 0
-   salary = 1;  
+   salary = 0; 
+   bracket = lower < salary; 
+   bracket(1) = 1; 
+else
+   % find the tax bracket 
+    lower_ii = lower < salary; 
+    upper_ii = upper >= salary; 
+    bracket = and(lower_ii, upper_ii);  
 end
 
-% find the tax bracket 
-lower_ii = lower < salary; 
-upper_ii = upper >= salary; 
-bracket = and(lower_ii, upper_ii); 
- 
 % calculate tax from previous brackets 
 % rate in bracket times upper limit of those brackets 
 prev_bracket_taxes = owed(bracket); 
@@ -24,7 +29,8 @@ prev_bracket_taxes = owed(bracket);
 % calculate tax from current bracket
 % rate in bracket times how much we exceed our bracket minimum by
 salary_in_bracket = salary - lower(bracket); 
-in_bracket_taxes =  salary_in_bracket*rate(bracket); 
+in_bracket_taxes =  salary_in_bracket*rate(bracket);
+
 total_taxes = prev_bracket_taxes + in_bracket_taxes; 
 
 end
--- tax_brackets.m
+++ tax_brackets.m
@@ -1,33 +1,47 @@
 
+% bracket tax rates 
 rate = [.1, .15, .25, .28, .33, .35, .3960];
 
+% personal exemption 
+pexemp = 4050;
+
+% the lower and upper salaries for each bracket for a single filer  
 slower = [0, 9325, 37950, 91900, 191650, 416700, 418400];
 supper = [9325, 37950, 91900, 191650, 416700, 418400, Inf];
+
+% amount owed from all previous brackets for each bracket for single filer
 sowed  = [0, 932.5, 5226.25, 18713.75, 46643.75, 120910.25, 121505.25];
+
+% the lower and upper salaries for each bracket for joint filers
+jlower = [0, 18650, 75900, 153100, 233350, 416700, 470700];
+jupper = [18650, 75900, 153100, 233350, 416700, 470700, Inf];
+
+% amount owed from all previous brackets for each bracket for joint filers
+jowed  = [0, 1865, 10452.50, 29752.50, 52222.50, 112728, 131628];
+
+% single filer deductable and PEASE limit 
 sdeduct = 6350;
 spep = 384000;
 
-pexemp = 4050;
-
-jlower = [0, 18650, 75900, 153100, 233350, 416700, 470700];
-jupper = [18650, 75900, 153100, 233350, 416700, 470700, Inf];
-jowed  = [0, 1865, 10452.50, 29752.50, 52222.50, 112728, 131628];
+% joint filer deductable and PEASE limit 
 jdeduct = 12700;
 jpep = 436300;
 
+% example salaries (in K)
 salary1 = 70;
 salary2 = 100;
 
+% salaries in dollars 
 salary1 = salary1*1000;
 salary2 = salary2*1000;
 jsalary = salary1 + salary2;
 
+% compute joint taxes 
 jtax= compute_tax_owed(jsalary, jlower, jupper, rate, jowed, jdeduct, jpep, pexemp);
 
+% compute single taxes 
 stax1 = compute_tax_owed(salary1, slower, supper, rate, sowed, sdeduct, spep, pexemp);
 stax2 = compute_tax_owed(salary2, slower, supper, rate, sowed, sdeduct, spep, pexemp);
-
-
 
 fprintf('Joint Tax           = $%d,%03d\n', ...
     floor(jtax/1000),round(rem(jtax, 1000)) );
@@ -35,7 +49,8 @@ fprintf('Joint Tax           = $%d,%03d\n', ...
 fprintf('Single Combined Tax = $%d,%03d\n', ...
     floor( (stax1 + stax2)/1000), round(rem(stax1 + stax2, 1000)) );
 
-%% Compute when to change from single to joint taxes
+%% Difference in Separately-Filed and Joint-Filed Taxes Owed 
+
 salaries = 1:5:200; % [10:10:100, 120, 150, 200, 250, 300, 400];
 
 [X,Y] = meshgrid(salaries,salaries);
@@ -49,13 +64,14 @@ for n = 1:NS
     
     %compute taxes
     jtax= compute_tax_owed(jsalary, jlower, jupper, rate, jowed, jdeduct, jpep, pexemp);
-    
     stax1 = compute_tax_owed(salary1, slower, supper, rate, sowed, sdeduct, spep, pexemp);
     stax2 = compute_tax_owed(salary2, slower, supper, rate, sowed, sdeduct, spep, pexemp);
     
+    % compute difference in taxes (separate minus joint) 
     Z(n) = stax1 + stax2 - jtax; 
 end
 
+%% Surface Plot 
 h = figure(1); clf; 
 h.Position = [99 112 804 587]; 
 
@@ -64,7 +80,7 @@ xlabel('first salary(K)');
 ylabel('second salary(K)'); 
 zlabel('separate taxes - joint taxes($)'); 
 
-
+% add hyperplane at Z = 0 for visualization 
 yp = get(gca,'Ylim');
 xp = get(gca,'Xlim');
 x1 = [ xp(1) xp(2) xp(2) xp(1)];
@@ -79,4 +95,3 @@ view([5.7 37.2]);
 filenm = 'joint_separate_file_tax_difference_by_salary'; 
 print(filenm, '-dpng'); 
 
-% legend('Location', 'best', {'joint', 'separate'}); 
